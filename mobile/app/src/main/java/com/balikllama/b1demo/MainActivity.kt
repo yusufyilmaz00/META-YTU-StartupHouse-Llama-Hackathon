92f2da903be680cb6a3909335947c906ae695a1d
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-10-24 21:51:24 +0300

    update: oluşturulan tema mainActivity'ye uygulandı.

--- mobile/app/src/main/java/com/balikllama/b1demo/MainActivity.kt
+++ mobile/app/src/main/java/com/balikllama/b1demo/MainActivity.kt
@@ -6,15 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.balikllama.b1demo.ui.components.BottomNavigationBar
+import com.balikllama.b1demo.ui.designsystem.AppTheme
 import com.balikllama.b1demo.ui.navigation.AppNavGraph
 import com.balikllama.b1demo.ui.theme.B1demoTheme
 
@@ -22,19 +25,32 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             val windowSizeClass = calculateWindowSizeClass(this)
-            val navController = rememberNavController()
 
-            B1demoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
-                    bottomBar = { BottomNavigationBar(navController) }
-                ) { innerPadding ->
-                    AppNavGraph(navController, modifier = Modifier.padding(innerPadding),windowSizeClass= windowSizeClass)
-                }
+            AppTheme {
+                MainScreen(windowSizeClass = windowSizeClass)
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen(windowSizeClass: WindowSizeClass) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
+        bottomBar = {
+            BottomNavigationBar(navController)
+        }
+    ) { innerPadding ->
+        AppNavGraph(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding),
+            windowSizeClass = windowSizeClass
+        )
     }
 }
 

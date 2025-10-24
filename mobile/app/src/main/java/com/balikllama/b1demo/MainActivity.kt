ded733d6dfd466d6a77258fe42eeb45cd7631055
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-10-24 20:34:27 +0300

    update: MainActivity navigasyon yapısıyla güncellendi

--- mobile/app/src/main/java/com/balikllama/b1demo/MainActivity.kt
+++ mobile/app/src/main/java/com/balikllama/b1demo/MainActivity.kt
@@ -8,22 +8,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.balikllama.b1demo.ui.components.BottomNavigationBar
+import com.balikllama.b1demo.ui.navigation.AppNavGraph
 import com.balikllama.b1demo.ui.theme.B1demoTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val windowSizeClass = calculateWindowSizeClass(this)
+            val navController = rememberNavController()
+
             B1demoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    bottomBar = { BottomNavigationBar(navController) }
+                ) { innerPadding ->
+                    AppNavGraph(navController, modifier = Modifier.padding(innerPadding),windowSizeClass= windowSizeClass)
                 }
             }
         }

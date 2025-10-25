d837bebf0120cad307db0a2cee4b8e13561a9070
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-10-25 10:34:58 +0300

    update:home view model ve top bar uygulandı

--- mobile/app/src/main/java/com/balikllama/b1demo/ui/screen/home/HomeScreen.kt
+++ mobile/app/src/main/java/com/balikllama/b1demo/ui/screen/home/HomeScreen.kt
@@ -2,18 +2,26 @@ package com.balikllama.b1demo.ui.screen.home
 
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
+    viewModel: HomeViewModel= viewModel(),
     windowSizeClass: WindowSizeClass
 ) {
+    val credits by viewModel.userCredits.collectAsState()
 
     HomeView(
         modifier = modifier,
         windowSizeClass = windowSizeClass,
+        creditInfo = credits.toString(),
+        onRefreshCredits = { viewModel.loadUserCredits() }
     )
 }
--- mobile/app/src/main/java/com/balikllama/b1demo/ui/screen/home/HomeView.kt
+++ mobile/app/src/main/java/com/balikllama/b1demo/ui/screen/home/HomeView.kt
@@ -7,16 +7,24 @@ import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.balikllama.b1demo.Greeting
+import com.balikllama.b1demo.ui.components.AppTopBar
 import com.balikllama.b1demo.ui.theme.B1demoTheme
 
 @Composable
 fun HomeView(
     modifier: Modifier = Modifier,
-    windowSizeClass: WindowSizeClass
+    windowSizeClass: WindowSizeClass,
+    creditInfo: String,
+    onRefreshCredits: () -> Unit = {},
 ) {
 
     B1demoTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Scaffold(modifier = Modifier.fillMaxSize(),
+            topBar = {
+                AppTopBar(title = "Home",
+                    creditInfo = creditInfo)
+            }
+        ) { innerPadding ->
             Greeting(
                 name = "HOME PAGE",
                 modifier = Modifier.padding(innerPadding)

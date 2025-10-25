77f4ca564687c831e36602b03874a397a003ef5e
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-10-25 18:04:09 +0300

    update: uygulama ikonu eklendi

--- mobile/app/src/main/java/com/balikllama/b1demo/di/NetworkModule.kt
+++ mobile/app/src/main/java/com/balikllama/b1demo/di/NetworkModule.kt
@@ -11,6 +11,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import com.balikllama.b1demo.BuildConfig
 import javax.inject.Singleton
 
 @Module
